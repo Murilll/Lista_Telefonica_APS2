3a3abe004cfb430b8c394b1709b6c3b4193322a3
Aluno <Aluno@SNCIC50419>
2023-05-26 19:28:56 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_Telefonica_APS2/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Telefonica_APS2
+{
+    internal class Class
+    {
+    }
+}

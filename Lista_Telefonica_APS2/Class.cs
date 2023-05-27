96e9161e1604d714d7cd9c00744fccd01db6db07
Murilll <murilosertorio25@gmail.com>
2023-05-26 21:46:09 -0300

    Create: create new functions

--- Lista_Telefonica_APS2/Class.cs
+++ Lista_Telefonica_APS2/Class.cs
@@ -1,12 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
-namespace Lista_Telefonica_APS2
+namespace Lista_Telefonica_APS2;
+
+public class Person
 {
-    internal class Class
+    public int Identifier { get; set; }
+    public string Name { get; set; }
+    public string Telephone { get; set; }
+
+    public Person(int identifier, string name, string telephone)
     {
+        this.Identifier = identifier;
+        this.Name = name;
+        this.Telephone = telephone;
+    }
+}
+
+public class Function
+{
+    public List<Person> Contact = new List<Person>();
+
+    string result = "This name does not exist";
+
+    public void AddPerson(Person people)
+    {
+        Contact.Add(people);
+    }
+
+    public void GetPerson()
+    {
+        foreach (var item in Contact)
+        {
+            System.Console.WriteLine($"ID: {item.Identifier}\nName: {item.Name}\nTelephone: {item.Telephone}\n");
+        }
+    }
+
+    public void SearchForFontactByPhone(string number)
+    {
+        foreach (var item in Contact)
+            if (item.Telephone == number)
+               Console.WriteLine($"ID: {item.Identifier}\nName: {item.Name}\nTelephone: {item.Telephone}\n");
+    }
+
+    public void SearchForFontactByName(string name)
+    {
+        foreach (var item in Contact)
+            if (item.Name == name)
+                Console.WriteLine($"ID: {item.Identifier}\nName: {item.Name}\nTelephone: {item.Telephone}\n");
+    }
+
+    public string DeleteContactsByNumber(string number)
+    {
+        foreach (var item in Contact)
+        {
+            if (item.Name == null)
+                return result;
+
+            else if (item.Telephone == number)
+            {
+                Console.WriteLine($"ID: {item.Identifier}\nName: {item.Name}\nTelephone: {item.Telephone}\nSuccessfully Deleted.");
+                Contact.Remove(item);
+            }
+        }
+
+        return result;
+    }
+
+    public string DeleteContactsByName(string name)
+    {
+        foreach (var item in Contact)
+        {
+            if (item.Name == null)
+                return result;
+
+            else if (item.Name == name)
+            {
+                Console.WriteLine($"ID: {item.Identifier}\nName: {item.Name}\nTelephone: {item.Telephone}\nSuccessfully Deleted.");
+                Contact.Remove(item);
+            }
+        }
+
+        return result;
     }
 }
--- Lista_Telefonica_APS2/Program.cs
+++ Lista_Telefonica_APS2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lista_Telefonica_APS2;
+
+Function Funções = new Function();
+
+Person people = new Person(1, "Murilo", "41999999");
+Console.WriteLine("");
+
+
+
+Funções.AddPerson(people);
+
+
+
+Funções.DeleteContactsByNumber("41999999");
+
+
+foreach (var item in Funções.Contact)
+{
+    Console.WriteLine(item.Name);
+}
+
+Funções.DeleteContactsByNumber("41999999");
+
+
+Funções.DeleteContactsByNumber("41999999");

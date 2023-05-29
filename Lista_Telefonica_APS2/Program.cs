ff582d32502d7aa058f59f892d9979d20790b7b9
Murilll <murilosertorio25@gmail.com>
2023-05-29 08:40:40 -0300

    Update: credentials have been changed

--- Lista_Telefonica_APS2/Program.cs
+++ Lista_Telefonica_APS2/Program.cs
@@ -6,6 +6,10 @@ Person people = new Person(1, "Murilo", "41999999");
 Person people1 = new Person(2, "fdsaf", "543654");
 Person people2 = new Person(3, "ghfsh", "543654");
 Person people3 = new Person(4, "hgtejeh", "435643");
+Person people4 = new Person(5, "uryth", "41999999");
+Person people11 = new Person(6, "htrwhtr", "543654");
+Person people22 = new Person(7, "jytw", "543654");
+Person people33 = new Person(8, "jywyw", "435643");
 
 Console.WriteLine("");
 
